0041246da5da16a582f1629ca59c343221989906
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-12-07 17:26:58 +0300

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''The module contains an addition function'''
+
+
+def add(a: float, b: float) -> float:
+    '''To add according to datatype and return their sum'''
+    return a + b
