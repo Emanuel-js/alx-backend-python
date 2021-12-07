d98b298668cfdb894f7a695bc454dc6478a2d692
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-12-07 17:54:40 +0300

    Create 9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''This module contains an element_length function'''
+from typing import Iterable, Sequence, Tuple, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''returns a list of tuple that iterates'''
+    return [(i, len(i)) for i in lst]
